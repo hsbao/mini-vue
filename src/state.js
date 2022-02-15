efb8de96f30748b233dcc3831dc5d2e80e22cb72
hongshibao <arvin.hong@corp.to8to.com>
2022-02-15 15:18:38 +0800

    proxy 属性代理

--- src/state.js
+++ src/state.js
@@ -1,7 +1,9 @@
 import { observe } from './observe/index'
 
+import { proxy } from './utils/index'
+
 export function initState(vm) {
-  const opts = vm.$options
+	const opts = vm.$options
 	// vue的数据来源：属性，方法，数据，计算属性，watch
 	if (opts.props) {
 		initProps(vm)
@@ -25,6 +27,11 @@ function initMethods(vm) {}
 function initData(vm) {
 	let data = vm.$options.data
 	data = vm._data = typeof data === 'function' ? data.call(vm) : data // data.call(vm) 为了保证在data里面使用的this指向当前实例
+
+	// 为了让用户更好的使用 我希望可以直接vm.xxx
+	for (let key in data) {
+		proxy(vm, '_data', key)
+	}
 
 	// 数据劫持，使用Object.defineProperty()给属性增加get和set方法
 	// 也就是MVVM模块，属性改变，得到通知，从而刷新页面（响应式原理）
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 /**
  * 判断是否是对象
- * @param {*} obj 
- * @returns 
+ * @param {*} obj
+ * @returns
  */
 export function isObject(obj) {
 	return typeof obj === 'object' && obj !== null
@@ -11,6 +11,18 @@ export function def(data, key, value) {
 	Object.defineProperty(data, key, {
 		enumerable: false, // 是否可以枚举
 		configurable: false, // 是否可以修改
-		value
+		value,
+	})
+}
+
+// 取值时实现代理效果
+export function proxy(vm, source, key) {
+	Object.defineProperty(vm, key, {
+		get() {
+			return vm[source][key]
+		},
+		set(newValue) {
+			vm[source][key] = newValue
+		},
 	})
 }
